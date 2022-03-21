52ffff9a574286fffdf9e58c27ec7ac087b0df8f
Pratool Gadtaula <5598110+Pratool@users.noreply.github.com>
2022-03-21 00:25:28 -0400

    wip: add naive threadpool

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from bottle import route, run, template, post, request
 import youtube_dl
 import threading
+from uuid import uuid4
 
 class thread(threading.Thread):
     def __init__(self, url):
         threading.Thread.__init__(self)
         self.url = url
+        self.id = str(uuid4())
 
     def run(self):
-        ydl_opts = {}
+        ydl_opts = {"outtmpl": f"{self.id}.%(ext)s"}
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([self.url])
 
 # Need to manage a threadpool outside of the scope of the routing definitions.
-thread1 = thread("")
+thread_pool = [thread("")] * 2
 
 @route('/hello/<name>')
 def index(name):
@@ -21,10 +23,17 @@ def index(name):
 
 @post('/post')
 def post_endpoint():
-    global thread1
+    global thread_pool
     url = request.json["url"]
-    thread1.url = url
-    thread1.start()
+    selected_thread = None
+    for t in thread_pool:
+        if not t.is_alive():
+            t = thread(url)
+            selected_thread = t
+            break
+    if selected_thread is None:
+        return {"error": "no resources available, please try again later"}
+    selected_thread.start()
     return request.json
 
 run(host='localhost', port=8080)
